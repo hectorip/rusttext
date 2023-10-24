d9b6b5296a766305d71f65c51ea7ac104362f622
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-10-23 23:08:10 -0600

    Main loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 // This was created by cargo init
 // Maybe I need to try cargo new instead
 fn main() {
-    println!("Hello, world🔥🦀!");
+    println!("Welcome to the teXt editor 🔥🦀!");
+    println!("This is a program I wrote to learn about Rust");
+    let mut input = String::new();
+    while std::io::stdin().read_line(&mut input).unwrap() > 0 {
+        println!("{}", input);
+    }
 }
