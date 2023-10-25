8d528614724e16691b24fdbae712ef95bcda902e
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-10-24 23:59:28 -0600

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@
 // Maybe I need to try cargo new instead
 fn main() {
     println!("Welcome to the teXt editor 🔥🦀!");
-    println!("This is a program I wrote to learn about Rust");
+    println!("This is a program I wrote to learn about Rust\n\n");
     let mut input = String::new();
     while std::io::stdin().read_line(&mut input).unwrap() > 0 {
-        println!("\n\n----------\n{}\n---------\n", input);
+        // println!("\n\n----------\n{}\n---------\n", input);  
     }
 }
