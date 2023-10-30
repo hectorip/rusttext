41f5ce87ff2f58f42f3c940254e1443a64f82b8f
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-10-29 23:44:45 -0600

    Not working experiment

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 // This was created by cargo init
-// Maybe I need to try cargo new instead
+
 fn main() {
-    println!("Welcome to the teXt editor 🔥🦀!");
+    println!("Welcome to the teXT editor 🔥🦀!");
+
     // println!("This is a program I wrote to learn about Rust\n\n");
 
     let mut input = String::new();
-    while std::io::stdin().read_line(&mut input).unwrap() > 0 {
-        println!("{}", input);
+    while std::io::stdin().read_line(&mut input).unwrap() > 0 && input[-1].trim() == "q"{
+        println!("Salir");
         input.clear();
     }
 }
