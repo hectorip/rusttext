ef7baf6995158a0b25544fb66acc663e35eb408a
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-10-31 23:57:41 -0600

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 // This was created by cargo init
 
+use std::io::Read;
+
 fn main() {
     println!("Welcome to the teXT editor 🔥🦀!");
 
     // println!("This is a program I wrote to learn about Rust\n\n");
 
-    let mut input = String::new();
-    while std::io::stdin().read_line(&mut input).unwrap() > 0 {
+    let mut input: []] = String::new();
+    while std::io::stdin().read_exact(&mut input).unwrap() > 0 {
         if input.trim() == "q" {
             println!("Salir");
             input.clear();
             break;
-
         } else {
             input.clear();
         }
